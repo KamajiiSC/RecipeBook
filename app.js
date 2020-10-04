12ef43b43a4c22350bd1afab9b6afab6671f5325
KamajiiSC <brandonlschaen@gmail.com>
2020-10-04 13:23:23 -0400

    imported the products file with its router

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+const productRoutes = require('./api/routes/products');
+
 app.use((req, res, next) => {
   res.status(200).json({
     message: 'Its working!'

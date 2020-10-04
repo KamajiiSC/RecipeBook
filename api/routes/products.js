3ea17987823d9de7cf3cb68b7904dd21aba51f04
KamajiiSC <brandonlschaen@gmail.com>
2020-10-04 13:29:02 -0400

    Setup code response and json message for GET and POST requests

--- api/routes/products.js
+++ api/routes/products.js
@@ -2,5 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
+  res.status(200).json({
+    message: 'Handling GET requests for /products'
+  })
+});
 
+router.post('/', (req, res, next) => {
+  res.status(200).json({
+    message: 'Handling POST requests for /products'
+  })
 });

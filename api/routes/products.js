a7acbbf323936ec848d1833717fdf9534b6e20cb
KamajiiSC <brandonlschaen@gmail.com>
2020-10-05 15:17:30 -0400

    setup example routes for get with an id and the patch and delete routes

--- api/routes/products.js
+++ api/routes/products.js
@@ -4,11 +4,39 @@ const router = express.Router();
 router.get('/', (req, res, next) => {
   res.status(200).json({
     message: 'Handling GET requests for /products'
-  })
+  });
 });
 
 router.post('/', (req, res, next) => {
   res.status(200).json({
     message: 'Handling POST requests for /products'
-  })
+  });
 });
+
+router.get('/:productId', (req, res, next)  => {
+  const id = req.params.productId;
+  if(id === 'special'){
+    res.status(200).json({
+      message: 'You got it',
+      id: id
+    })
+  }
+  else{
+    res.status(200).json({
+      message: 'You passed an ID'
+    })
+  }
+});
+
+router.patch('/:productId', (req, res, next) => {
+  res.status(200).json({
+    message: 'Updated product'
+  });
+});
+router.delete('/:productId', (req, res, next) => {
+  res.status(200).json({
+    message: 'Deleted product'
+  });
+});
+
+module.exports = router;

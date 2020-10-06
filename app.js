769557f631be7327056762b80529c5f2a1a94340
KamajiiSC <brandonlschaen@gmail.com>
2020-10-06 17:45:01 -0400

    Set a route to catch anything uncaught and return 404 error. Created another route for all errors that returns your error message regardless of where the error came from

--- app.js
+++ app.js
@@ -8,4 +8,19 @@ app.use(morgan('dev'));
 
 app.use('/products', productRoutes);
 
+app.use((req, res, next) => {
+  const error = new Error('Note Found');
+  error.status(404);
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  })
+});
+
 module.exports = app;

e484c2b2d1612c079c478b57ec3536fd91ca913b
KamajiiSC <brandonlschaen@gmail.com>
2020-10-08 15:43:51 -0400

    Imported Product and mongoose, setup a product post

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+const Product = require('../models/product');
 
 router.get('/', (req, res, next) => {
   res.status(200).json({
@@ -8,9 +11,10 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
-  const product = {
-    name: req.body.name
-  }
+  const product = new Product({
+    _id: new mongoose.Types.ObjectId(),
+    name: req.body.name,
+  });
   res.status(201).json({
     message: 'Handling POST requests for /products',
     createdProduct: product

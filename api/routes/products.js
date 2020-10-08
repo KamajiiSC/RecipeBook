4692f4034bc2ed8696b9936f1f4abfa57e90d6bc
KamajiiSC <brandonlschaen@gmail.com>
2020-10-08 18:45:35 -0400

    Setup post requests for main dir

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const product = require('../models/product');
 
 const Product = require('../models/product');
 
 router.get('/', (req, res, next) => {
-  res.status(200).json({
-    message: 'Handling GET requests for /products'
-  });
+  Product.find()
+    .exec()
+    .then(docs => {
+      console.log(docs)
+      res.status(200).json(docs);
+    })
+    .catch(err => {
+      res.status(500).json({error: err})
+    })
 });
 
 router.post('/', (req, res, next) => {

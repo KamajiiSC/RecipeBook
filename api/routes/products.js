f13f792ea52b45453b7154dbd67b68192ca6d54a
KamajiiSC <brandonlschaen@gmail.com>
2020-10-08 18:38:05 -0400

    Setup delete request with deleteOne

--- api/routes/products.js
+++ api/routes/products.js
@@ -38,7 +38,13 @@ router.get('/:productId', (req, res, next)  => {
     .exec()
     .then(doc => {
       console.log(doc);
-      res.status(200).json(doc);
+      if(doc){
+        res.status(200).json(doc);
+      }
+      else{
+        res.status(404).json({message: 'No valid entries match that ID'});
+      }
+
     })
     .catch(err => {
       console.log(err);
@@ -51,10 +57,18 @@ router.patch('/:productId', (req, res, next) => {
     message: 'Updated product'
   });
 });
+
 router.delete('/:productId', (req, res, next) => {
-  res.status(200).json({
-    message: 'Deleted product'
-  });
+  const id = req.params.productId;
+  Product.deleteOne({_id: id})
+    .exec()
+    .then(result => {
+      res.status(200).json(result);
+    })
+    .catch(err =>{
+      console.log(err);
+      res.status(500).json({error: err});
+    })
 });
 
 module.exports = router;
